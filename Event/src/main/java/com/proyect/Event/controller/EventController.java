6f20a4e079ebd992a9da81bf18c221af14c7aa13
alumno <alumno@Alumno18>
2022-01-28 12:51:45 +0100

    errores

--- Event/src/main/java/com/proyect/Event/controller/EventController.java
+++ Event/src/main/java/com/proyect/Event/controller/EventController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
 import com.proyect.Event.Services.EventServices;
 import com.proyect.Event.controller.exceptions.EventNotFound;
 import com.proyect.Event.model.Event;
@@ -41,8 +40,6 @@ public class EventController {
 	@Autowired
 	private EventServices eventServices;
 
-
-	
 	@Operation(summary = "List of all available events", description = "returns a json with all events in the database", tags = {
 			"Event" })
 	@GetMapping("/events")
@@ -51,13 +48,11 @@ public class EventController {
 		return eventServices.findAll();
 	}
 
-	
 	@Operation(summary = "Save a event", description = "inserts an event to the database", tags = { "Event" })
 	@PutMapping
 	public Event save(Event event) {
 		return eventServices.save(event);
 	}
-	
 
 	@Operation(summary = "Add a new eventt", description = "Add a new eventt", tags = { "Event" })
 	@PostMapping("/add")
@@ -70,16 +65,14 @@ public class EventController {
 		return ResponseEntity.ok("EVENT HAS BEEN CREATED \n" + event);
 	}
 
-	
-			@Operation(summary = "Delete a event", description = "Deletes an event by its code", tags = { "Event" })
-			@DeleteMapping("/deleteEvent/{code}")
-			public ResponseEntity<?> deleteUser(@PathVariable("code") String code) {
-				log.info("------ deleting event) " + code);		
-					eventServices.deleteEvent(code);
-					log.info("------ DELETED ");
-					return ResponseEntity.ok("DELETED");
-			
-		
+	@Operation(summary = "Delete a event", description = "Deletes an event by its code", tags = { "Event" })
+	@DeleteMapping("/deleteEvent/{code}")
+	public ResponseEntity<?> deleteUser(@PathVariable("code") String code) {
+		log.info("------ deleting event) " + code);
+		eventServices.deleteEvent(code);
+		log.info("------ DELETED ");
+		return ResponseEntity.ok("DELETED");
+
 	}
 
 	// Actualizar juego
@@ -89,18 +82,20 @@ public class EventController {
 		eventServices.deleteEvent(code);
 		eventServices.save(event);
 	}
-	
-	//Listar eventos por género
-	@Operation(summary = "List events by genre", description = "returns a json with all events by genre in the database", tags = { "Event" })
+
+	// Listar eventos por género
+	@Operation(summary = "List events by genre", description = "returns a json with all events by genre in the database", tags = {
+			"Event" })
 	@GetMapping("/events/genre/{genre}")
 	public List<Event> findByGenre(@PathVariable String genre) {
 		return eventServices.findByGenre(genre);
 	}
-	
-	//Listar eventos por nombre
-	@Operation(summary = "List events by name", description = "returns a json with all events by name in the database", tags = { "Event" })
+
+	// Listar eventos por nombre
+	@Operation(summary = "List events by name", description = "returns a json with all events by name in the database", tags = {
+			"Event" })
 	@GetMapping("/events/name/{name}")
-	public List<Event> findByName(@PathVariable String name){
+	public List<Event> findByName(@PathVariable String name) {
 		return eventServices.findByName(name);
 	}
 }
--- Event/src/main/java/com/proyect/Event/controller/exceptions/EventNotFound.java
+++ Event/src/main/java/com/proyect/Event/controller/exceptions/EventNotFound.java
@@ -1,9 +1,6 @@
 package com.proyect.Event.controller.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class EventNotFound extends RuntimeException{
 
 	private static final long serialVersionUID = 1L;
@@ -11,7 +8,7 @@ public class EventNotFound extends RuntimeException{
 	public EventNotFound () {
 		super("FAILED OPERATION: Item DOES NOT exist");
 	}
-	public EventNotFound (String code) {
-		super("FAILED OPERATION: Item DOES NOT exit --code number: "+ code);
+	public EventNotFound (String name) {
+		super("FAILED OPERATION: Item DOES NOT exit --code number: "+ name);
 	}
 }
